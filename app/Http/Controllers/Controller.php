6c6d4a9b1b75659fca51041657da008e33c111a1
Jakub <holikj1@gmail.com>
2025-04-02 12:37:44 +0200

    Added settings page

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\View\View;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    function settings(): View{
+        return view('settings');
+    }
 }
